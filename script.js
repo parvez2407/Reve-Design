65ff7bb7e4d17be905c6fc4ff189efe129896c33
parvez2407 <parvezkhan811425@gmail.com>
2025-01-02 12:42:01 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+// Auto-slide every 5 seconds
+setInterval(showNextSlide, 5000);
